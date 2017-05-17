01653b075003e028f03e34c5fad35445f6533b96
jclachance <jelachance@eng.ucsd.edu>
2017-05-16 19:01:16 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='biomass',
+      version='0.1',
+      description='Package to generate biomass coefficients from OMICs data',
+      url='https://github.com/jclachance/biomass/',
+      author='Jean-Christophe Lachance',
+      author_email='jelachance@eng.ucsd.edu',
+      license='MIT',
+      packages=['biomass'],
+      zip_safe=False)
