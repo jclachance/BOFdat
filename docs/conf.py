701ca6d3c3172ff9b48a29334b9400b915af4d54
jclachance <jelachance@eng.ucsd.edu>
2017-11-22 15:55:30 -0800

    post release modifications

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 #
 import os
 import sys
-sys.path.append(0,os.path.abspath('.'))
-sys.path.append(0,os.path.abspath('../'))
+sys.path.insert(0,os.path.abspath('.'))
+sys.path.insert(0,os.path.abspath('../'))
 
 
 # MOCK STUFF
