2e61c4b884b6ce22e17316ab0b0b345aeeac342d
jclachance <jelachance@eng.ucsd.edu>
2018-06-13 16:57:59 -0700

    Test with step1 only

--- BOFdat/__init__.py
+++ BOFdat/__init__.py
@@ -1,13 +1,9 @@
-from BOFdat import (step1,step2,step3)
+from BOFdat import (step1)
 from BOFdat.core import (dna,
 	rna,
 	protein,
 	lipid,
-	maintenance,
-	coenzymes_and_ions,
-	initial_population,
-	chose_metab,
-	metab_end_goals)
-from BOFdat.util import update,dijkstra
+	maintenance)
+from BOFdat.util import update
 
 __version__ = "0.4.0"
