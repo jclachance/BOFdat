560b08b6b3942a28bf7229e48c0ac1254882df69
jclachance <jelachance@eng.ucsd.edu>
2018-06-13 16:38:21 -0700

    Remove sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -76,10 +76,9 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
-    'nbsphinx'
+    'sphinx.ext.autosummary'
 ]
-#    'autoapi.extension',
+#  'nbsphinx'  'autoapi.extension',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
 
