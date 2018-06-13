fae6edba6b6acee7bff4d61d90a4375d8dbf2f2c
jclachance <jelachance@eng.ucsd.edu>
2018-06-13 16:30:37 -0700

    Fixing docs...

--- docs/conf.py
+++ docs/conf.py
@@ -77,10 +77,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
-    'autoapi.extension',
     'nbsphinx'
 ]
-
+#    'autoapi.extension',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
 
