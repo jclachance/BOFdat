6e2802dc7f2e5cb9136ecf8011c673b7e703932e
jclachance <jelachance@eng.ucsd.edu>
2018-06-13 17:00:37 -0700

    Test only step1

--- BOFdat/core/__init__.py
+++ BOFdat/core/__init__.py
@@ -3,7 +3,7 @@ import rna
 import protein
 import lipid
 import maintenance
-import coenzymes_and_ions
-import initial_population
-import chose_metab
-import metab_end_goals
+#import coenzymes_and_ions
+#import initial_population
+#import chose_metab
+#import metab_end_goals
