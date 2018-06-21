9b2d7a6a6d4bce0fd60b11f7f18d71a18a5b22f4
jclachance <jelachance@eng.ucsd.edu>
2018-06-20 17:49:49 -0700

    Fixing docs

--- BOFdat/__init__.py
+++ BOFdat/__init__.py
@@ -1,11 +1,7 @@
-from BOFdat import step1,step2,step3
-from BOFdat.core import (dna,
-	rna,
-	protein,
-	lipid,
-	maintenance)
+from BOFdat import step1,step2#,step3
+from BOFdat.core import dna,rna,protein,lipid,maintenance
 from BOFdat.core import coenzymes_and_ions
-from BOFdat.core import initial_population
+#from BOFdat.core import initial_population
 from BOFdat.util import update
 
 __version__ = "0.4.0"
