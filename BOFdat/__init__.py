bc6b299c73206c863492b5e2811c3b414e4ab120
jclachance <jelachance@eng.ucsd.edu>
2018-06-20 18:10:19 -0700

    Fixing docs

--- BOFdat/__init__.py
+++ BOFdat/__init__.py
@@ -1,7 +1,15 @@
-from BOFdat import step1,step2,step3
-from BOFdat.core import dna,rna,protein,lipid,maintenance
+from BOFdat import step1
+from BOFdat import step2
+from BOFdat import step3
+from BOFdat.core import dna
+from BOFdat.core import rna
+from BOFdat.core import protein
+from BOFdat.core import lipid
+from BOfdat.core import maintenance
 from BOFdat.core import coenzymes_and_ions
-from BOFdat.core import initial_population,find_metabolites,cluster_metabolites
+from BOFdat.core import initial_population
+from BOFdat.core import find_metabolites
+from BOFdat.core import cluster_metabolites
 from BOFdat.util import update
 
 __version__ = "0.4.0"
