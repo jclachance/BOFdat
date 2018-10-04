6ff928d4e30fd6fd7d1a618d16e3f5c91c72a683
jclachance <jelachance@eng.ucsd.edu>
2018-10-04 13:12:41 -0700

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='BOFdat',
-      version='0.1.5',
-      download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.5.tar.gz',
+      version='0.1.6',
+      download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.6.tar.gz',
       description='Package to generate biomass objective function stoichiometric coefficients from experimental data',
       url='https://github.com/jclachance/BOFdat/',
       author='Jean-Christophe Lachance',
