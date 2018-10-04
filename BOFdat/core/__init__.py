3825a8f0b0fba57f8ea92013bd9a0c1d01754a36
jclachance <jelachance@eng.ucsd.edu>
2018-10-04 11:56:16 -0700

    core and util __init__.py updated

--- BOFdat/core/__init__.py
+++ BOFdat/core/__init__.py
@@ -1,10 +1,1 @@
-import dna
-import rna
-import protein
-import lipid
-import maintenance
-import coenzymes_and_ions
-import initial_population
-import metab_end_goals
-import group_end_goals
 from BOFdat.util import update
